2f13028c92f02308178f2f0ea07a4095d0f0ec82
UniBreakfast <unibreakfast1@gmail.com>
2021-12-11 16:59:54 +0200

    migrate initial layout to react

--- src/App.js
+++ src/App.js
@@ -4,20 +4,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main id="gameBox">
+        <div id="cards">
+          <div class="card"></div>
+          <div class="card"></div>
+          <div class="card"></div>
+          <div class="card"></div>
+          <div class="card"></div>
+          <div class="card"></div>
+          <div class="card"></div>
+          <div class="card"></div>
+          <div class="card"></div>
+
+          <div id="inputBar">
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+          </div>
+        </div>
+        <div id="toolbar">
+          <div class="ingame">
+            <h3>Remember the card content you've seen</h3>
+            <button id="submit" onclick="gameBox.className=''">Submit solution</button>
+          </div>
+
+          <div class="inmenu">
+            <button id="start" onclick="gameBox.className='ingame'">
+              Start the game
+            </button>
+            <select id="difficultySelect">
+              <option value="4">4 seconds</option>
+              <option value="7">7 seconds</option>
+              <option value="10">10 seconds</option>
+              <option value="12">12 seconds</option>
+              <option value="15">15 seconds</option>
+            </select>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
